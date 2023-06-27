738ef1366a52df2fcb8534f6073bb3d6faaf455d
MoathDawahreh <moath.dawahreh@gmail.com>
2023-06-27 23:07:32 +0300

    performance improvments

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
-const scriptFolderPath = path.dirname(__filename);
-const folderPath = path.join(scriptFolderPath, "files");
-console.log(`folderPath: ${folderPath}`);
+async function moveFilesToSubfolders() {
+  const scriptFolderPath = path.dirname(__filename);
+  const folderPath = path.join(scriptFolderPath, "files");
+  console.log(`folderPath: ${folderPath}`);
 
-fs.readdir(folderPath, (err, files) => {
-  if (err) {
-    console.log(`Failed to read content ----> ${err}`);
-    return;
-  }
+  try {
+    console.time("Execution Time");
+    const files = await fs.readdir(folderPath);
+    const fileGroups = {};
 
-  files.forEach((file) => {
-    const baseName = path.basename(file);
-    const firstWord = baseName.split("-")[0];
-    const subfolderPath = path.join(folderPath, firstWord);
+    for (const file of files) {
+      const baseName = path.basename(file);
+      const firstWord = baseName.split("-")[0];
+      const subfolderPath = path.join(folderPath, firstWord);
 
-    if (!fs.existsSync(subfolderPath)) {
-      fs.mkdirSync(subfolderPath);
+      if (!fileGroups[firstWord]) {
+        fileGroups[firstWord] = [];
+        await fs.mkdir(subfolderPath).catch((err) => {
+          console.error(`Failed to create subfolder ${firstWord}: ${err}`);
+        });
+      }
+
+      const sourceFilePath = path.join(folderPath, baseName);
+      const destinationFilePath = path.join(subfolderPath, baseName);
+      await fs.rename(sourceFilePath, destinationFilePath).catch((err) => {
+        console.error(`Failed to move the file ${baseName}: ${err}`);
+      });
+
+      fileGroups[firstWord].push(baseName);
     }
 
-    const sourceFilePath = path.join(folderPath, baseName);
-    const destinationFilePath = path.join(subfolderPath, baseName);
-    fs.rename(sourceFilePath, destinationFilePath, (err) => {
-      if (err) {
-        console.error(`Failed to move the file: ${err}`);
-      } else {
-        console.log(
-          `moving the file ${baseName} to subfolder ${firstWord} ...`
-        );
-      }
-    });
-  });
-});
+    console.log("Files moved successfully.");
+
+    // Log the number of files in each subfolder
+    for (const [firstWord, files] of Object.entries(fileGroups)) {
+      console.log(`Subfolder ${firstWord}: ${files.length} files`);
+    }
+
+    console.timeEnd("Execution Time");
+  } catch (err) {
+    console.log(`Failed to read content: ${err}`);
+  }
+}
+
+moveFilesToSubfolders();
